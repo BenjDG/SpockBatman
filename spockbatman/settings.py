4e060fc115a626421d599d7b8e5e6bbff8307ea1
Benjamin Galloway <ben@Benjamins-MacBook-Air.local>
2023-01-02 14:09:43 -0600

    change settings to serve static files

--- spockbatman/settings.py
+++ spockbatman/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
